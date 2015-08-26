5db5c7f24cae50b18133bd46078703e523d5799b
Travis Johnston & Kendra Ash <student@epicodus.com>
2015-08-26 09:26:36 -0700

    create ingredients table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826162128) do
+ActiveRecord::Schema.define(version: 20150826162456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20150826162128) do
   create_table "dishes", force: :cascade do |t|
     t.string "name"
     t.string "category"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
   end
 
 end
